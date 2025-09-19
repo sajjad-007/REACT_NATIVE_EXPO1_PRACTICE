7c073a3f9438e9ccd44d070ba0b00d661c64fff1
sajjad-007 <sajjadhossain8123@gmail.com>
2025-09-19 23:06:40 +0600

    tmdb movie fetch api, useFetch custom hook created

---
+++ services/useFetch.ts
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react';
+
+//<T>(fetchFunction: () => Promise<T>) means “I accept a fetch function that returns a promise of some type T, and I’ll make sure to return that same type.”
+const useFetch = <T>(theFunction: () => Promise<T>, autoFetch: true) => {
+  const [data, setData] = useState<T | null>(null);
+  const [error, setError] = useState<Error | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchData = async () => {
+    try {
+      setIsLoading(true);
+      setError(null);
+      const results = await theFunction();
+      setData(results);
+    } catch (err) {
+      setError(
+        //In JavaScript and React Native, Error is a built-in class that represents an error object.
+        //instanceof checks if a value is an instance of a specific class.
+        err instanceof Error ? err : new Error('Error from useFect fetchData')
+      );
+      setIsLoading(false);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const reset = () => {
+    setData(null);
+    setError(null);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    if (autoFetch) {
+      fetchData();
+    }
+  }, []);
+
+  return { data, isLoading, error, reset, refetch: fetchData };
+};
+
+export = { useFetch };
